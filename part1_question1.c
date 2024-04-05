34151318643d7a283d223c5c975eb7cb367f378b
Ashley Cen <ngalam.cen@stonybrook.edu>
2024-04-04 23:07:01 -0400

    My first submission

--- part1_question1.c
+++ part1_question1.c
@@ -1,1 +1,110 @@
 
+//Ashley Cen (115796180)
+#include <stdio.h>
+#include <stdlib.h>
+
+int my_isalpha (char ch )  
+{
+	if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+	{
+		return 1; 
+	}
+	else 
+	{
+		return 0; 
+	}
+	
+}
+
+int my_isdigit (char ch) // return 1 if parameter is digit, 0 for other wise 
+{
+	ch = ch - '0'; 
+	if (ch >= 0 && ch <= 9)
+	{
+		return 1; 
+	}
+	else
+	{
+		return 0; 
+	}
+	
+}
+
+int my_is_upper (char ch)
+{
+	if (ch >= 'A' && ch <= 'Z')
+	{
+		return (1); 
+	}
+	else if (ch >= 'a' && ch <= 'z')
+	{
+		return (0); 
+	}
+}
+
+char my_to_upper (char ch)
+{
+	if (ch >= 'a' && ch <= 'z')
+	{
+	   ch = ch - 32; 
+	   return (ch); 
+	}
+	else
+	{
+		return (ch); 
+	}
+}
+
+
+int main ()
+{
+	char c; 
+	printf("Enter a character: \n");
+	scanf("%c", &c); 
+	getchar(); 
+	
+	if (my_isalpha(c) == 1)
+	{
+		printf("This is a letter.\n"); 
+		
+		if (my_is_upper (c) == 1)
+		{
+			printf("This is an uppercase letter.\n");
+		}
+		else if (my_is_upper(c) == 0)
+		{
+			printf("This is not an uppercase letter. \n");
+			c = my_to_upper (c); 
+			printf ("Uppercase Version: %c \n", c); 
+			
+		}
+	
+	}
+	
+	if (my_isdigit (c) == 1)
+	{
+		printf("This is a digit.\n"); 
+	}
+	else
+	{
+		printf("This is not a digit.\n"); 
+	}
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- part2_question1.c
+++ part2_question1.c
@@ -1,1 +1,114 @@
+//Ashley Cen (115796180)
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 32
+
+
+int my_strlen (char str[])
+{
+	int i = 0; 
+	while (str[i] != '\0')
+	{
+		i++; 
+	}
+	return i; 
+}
+int my_strcmp (char one[], char two[])
+{
+	int i = 0; 
+	int a = 0, b = 0; 
+	for (i= 0; i < (my_strlen(one)) || i < (my_strlen(two)); i++)
+	{
+		a = one[i] - '0'; 
+		b = two[i] - '0'; 
+		
+		if (a != b)
+		{
+			return 0; 
+		}
+	}
+	return 1; 
+	
+}
+
+int my_strncmp (char one [], char two[], int n)
+{
+	int i = 0; 
+	int a = 0, b = 0; 
+	for (i = 0; i < n; i++)
+	{
+		a = one[i] - '0'; 
+		b = two[i] - '0'; 
+		
+		if (a != b)
+		{
+			return 0; 
+		}
+	}
+	return 1; 
+}
+
+char *my_strcpy (char one[], char two[])
+{
+	char copy; 
+	int i = 0, j = 0; 
+	
+	while (one[i] != '\0'){
+		two[i] = one [i]; 
+	}
+	
+	for (j = i; j < my_strlen(two); j++)
+	{
+		two[j] = ' '; 
+	}
+	
+	return two; 
+}
+
+
+char *my_reverse (char one[], char two[]) //unfinished
+{
+	int i = 0; 
+	int length =  my_strlen(one);
+}
+
+
+
+int main ()
+{
+	char first [MAX] = {'m', 'a', 'g', 'n', 'e', 't', 'i', 'c', '\0' }; 
+	char second [MAX] = {'m', 'n', 'g', 'n', 'e', 't', 'i', 'c', '\0' }; 
+
+
+	//test my_strcmp ()
+	if (my_strcmp (first, second) == 1)
+	{
+		printf("The strings are equal.\n");
+    }
+	else if (my_strcmp (first, second) == 0)
+	{
+		printf("The strings are not equal.\n"); 
+	}
+	
+	//test my_strncmp()
+	if (my_strncmp (first, second, 3) == 1)
+	{
+		printf("The first 3 characters are equal.\n");
+    }
+	else if (my_strncmp (first, second, 3) == 0)
+	{
+		printf("The first 3 characters are not equal.\n"); 
+	}
+	
+	//testing my_strcat()
+	
+	
+	
+
+	
+	
+
+}
+
+
 
